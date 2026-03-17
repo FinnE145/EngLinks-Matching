17cff2869f7cae4b93ba7dcfb4bc076b78eb54a7
Truman Hui <truman.hui@queensu.ca>
2026-03-16 20:35:56 -0400

    best_tutor function "completed"

--- CoreAlg/matching.py
+++ CoreAlg/matching.py
@@ -1,6 +1,6 @@
 from data_utilities import load_excel, shelve_data, load_shelve
 
-def best_tutor(student_data: dict, tutors: dict) -> str | None:
+def best_tutor(student_data: dict, tutor1_id:int, tutor1: dict, tutor2_id: int, tutor2: dict) -> str | None:
     """
     Break a tie between multiple tutors with a scoring algorithm
 
@@ -11,7 +11,32 @@ def best_tutor(student_data: dict, tutors: dict) -> str | None:
     Returns:
         The ID of the best tutor, or ``None`` if no suitable tutor is found.
     """
-    pass
+    score1 = 0
+    score2 = 0
+
+    student_availability = student_data["availability"]
+
+    for day in student_availability:
+        if day in tutor1["availability"]:
+            score1 += 1
+    
+    for day in student_availability:
+        if day in tutor2["availability"]:
+            score2 += 1
+
+    if score1 > score2:
+        return tutor1_id
+
+    if score2 > score1:
+        return tutor2_id    
+    
+    if tutor1["students"] < tutor2["students"]:
+        return tutor1_id
+    
+    if tutor2["students"] < tutor1["students"]:
+        return tutor2_id
+    
+    return None
 
 def match(student_data: dict, tutors: dict) -> str | None:
     """
